6561f0c938bc6e565691fd39a2dc3d6a11e3faf9
Luta George Gabriel <luta.george.gabriel@gmail.com>
2018-05-09 13:48:10 +0300

    added gauge icon

--- appHomeCredit/screens/expenses/ExpensesScreen.js
+++ appHomeCredit/screens/expenses/ExpensesScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Chart from './components/Chart'
 import {Text, View, FlatList} from 'react-native';
 import Expense from './components/Expense'
+import Entypo from "react-native-vector-icons/Entypo";
+
 import {actions, colors, percentages, expenses} from "./components/data/ExpensesData";
 
 const style = {
@@ -9,13 +11,17 @@ const style = {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        top: 0, left: 0,
-        right: 0, bottom: 0
+        backgroundColor: '#A6DCF7'
     },
     text: {
         fontSize: 18,
         fontWeight: "400"
-    }
+    },
+    rowData: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center"
+    },
 };
 
 const renderItem = ({item, index}) => {
@@ -24,6 +30,10 @@ const renderItem = ({item, index}) => {
 
 export const ExpensesScreen = () => (
     <View style={style.container}>
+        <View style={style.rowData}>
+            <Entypo name="gauge" size={50} color="#E21932" />
+            <Text styl={style.text}>Dashboard</Text>
+        </View>
         <Chart/>
         <FlatList
             data={expenses}

50f39aed867ff81ac5d27066fbb7997cf6de2b4d
Luta George Gabriel <luta.george.gabriel@gmail.com>
2018-05-09 19:21:17 +0300

    created other data for actions

---
+++ appHomeCredit/screens/other/components/data/OthersData.js
@@ -0,0 +1,28 @@
+export const sections = [
+    {
+        title: '',
+        data: [
+            {
+                icon: "paypal",
+                service: 'Make payments',
+                code: '#2980b9',
+            },
+            {
+                icon: "calendar",
+                service: 'Schedule payment',
+                code: '#2980b9'
+            },
+            {
+                icon: "credit",
+                service: 'Exchange rate',
+                code: '#2980b9'
+            },
+            {
+                icon: "credit-card",
+                service: 'Request new credit line',
+                code: '#2980b9'
+            },
+
+        ]
+    },
+];
